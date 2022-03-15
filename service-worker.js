34df59564c4fbe77985e7c6106b2cfd925a9bc1d
gabGodiskova <gabgodiskova@gmail.com>
2022-03-15 16:06:26 +0000

    Deploying to docs from @ books-are-next/jirous-magorovy-labuti-pisne@e8bc935e8b0577d40df191de6424d61e79701bef 🚀

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 
 /* global self, caches, fetch */
 
-const CACHE = 'cache-1118350';
+const CACHE = 'cache-e8bc935';
 
 self.addEventListener('install', e => {
   e.waitUntil(precache()).then(() => self.skipWaiting());
